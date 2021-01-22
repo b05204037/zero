121a0305a8e79e4813eba49db12b62aca37caf86
b05204037 <b05204037@ntu.edu.tw>
2021-01-23 01:05:02 +0800

    add ir state

--- hw_pwm.py
+++ hw_pwm.py
@@ -3,35 +3,55 @@ import RPi.GPIO as GPIO
 import time
 import json
 
+# 
 pwm_info_filepath = './pwm_info.json'
 PWM_LED_PIN = 13
 PWM_FREQ = 2500
 UV_PIN = 21
-
+IR_PIN = 20
+#  pin20(ir) pin21(uv)
+#            gnd  
 GPIO.setmode(GPIO)
 GPIO.setup(UV_PIN, GPIO.OUT)
+GPIO.setup(IR_PIN, GPIO.OUT)
 pi = pigpio.pi()
 
 def inverse(led_int):
     return 100 - led_int
 
-#try:
-while True:
-    f = open(pwm_info_filepath)
-    pwm_info = json.load(f)
-    led_int = int(pwm_info['led'])
-    uv_int = pwm_info['uv']
-    
-    # continue here for the uv signal
-    if uv_int == "on" or uv_int == "true":
+def uv_output(uv_state):
+    if uv_state == "1" or uv_state == "true":
        GPIO.output(UV_PIN, GPIO.HIGH)
-    else :
+        return 'uv is on'
+    else:
        GPIO.output(UV_PIN, GPIO.LOW)
-    
-    # now use the inverse hardware
-    led_inverse = inverse(led_int)
-    pi.hardware_PWM(PWM_LED_PIN, PWM_FREQ, led_inverse*10000)
-    time.sleep(0.1)
+       return 'uv is down'
+
+def ir_output(ir_state):
+    if ir_state == "true":
+        GPIO.output(IR_PIN, GPIO.HIGH)
+        return 'ir is up'
+    else: 
+        GPIO.output(IR_PIN, GPIO.LOW)
+        return 'ir is down'
+try:
+    while True:
+        f = open(pwm_info_filepath)
+        pwm_info = json.load(f)
+        led_int = int(pwm_info['led'])
+        uv_int = pwm_info['uv']
+        ir_int = pwm_info['ir']
+
+        # continue here for the uv signal
+        uv_status = uv_output(uv_int)
+        print(uv_status)
+        ir_status = ir_output(ir_int)
+        print(ir_status)
+
+        # now use the inverse hardware
+        # led_inverse = inverse(led_int)
+        pi.hardware_PWM(PWM_LED_PIN, PWM_FREQ, led_int*10000)
+        time.sleep(0.1)
     
 #finally:
     #pi.set_mode(PWM_LED_PIN, pigpio.INPUT)
--- mqtt_pwm_client.py
+++ mqtt_pwm_client.py
@@ -2,33 +2,36 @@ import paho.mqtt.client as mqtt
 import json
 
 broker_ip = '172.20.10.2'
+farm_num = 'farm4'
 
 f = open('./pwm_info.json', 'r')
-mamual_state = json.load(f)
+
+state = json.load(f)
 f.close()
 
 def on_connect(client, userdata, flags, rc):
     print('Connect with the result code ' + str(rc))
-    client.subscribe('farm4/zero/led')
-    client.subscribe('farm4/zero/uv')
-    client.subscribe('farm3/ai_signal_zero/node1')
+    client.subscribe(farm_num + '/zero/led')
+    client.subscribe(farm_num + '/zero/uv')
+    client.subscribe(farm_num + '/zero/ir')
 
 def on_message(client, userdata, msg):
     topic = msg.topic
     content = msg.payload.decode('utf-8')
-    if topic == 'farm3/ai_signal_zero/node1':
+    if topic == farm_num + '/zero/ir':
         f = open('./pwm_info.json', 'w')
-        json.dump(eval(content), f)
+        state['ir'] = content
+        json.dump(state, f)
         f.close()
-    if topic == 'farm4/zero/led':
+    if topic == farm_num + '/zero/led':
         f = open('./pwm_info.json', 'w')
-        mamual_state['led'] = content
-        json.dump(mamual_state, f)
+        state['led'] = content
+        json.dump(state, f)
         f.close()
-    if topic == 'farm4/zero/uv':
+    if topic == farm_num + '/zero/uv':
         f = open('./pwm_info.json', 'w')
-        mamual_state['uv'] = content
-        json.dump(mamual_state, f)
+        state['uv'] = content
+        json.dump(state, f)
         f.close()
     
     print('topic : ' + topic + ' msg : ' + content)
